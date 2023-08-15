1244f91534ab6f396022972efa2bf8b737fa2b74
dnwor <dnwor@admin>
2023-08-15 09:59:05 +0900

    프로젝트 시작합니다

---
+++ jaewoo/src/main/java/com/example/jaewoo/JaewooApplication.java
@@ -0,0 +1,13 @@
+package com.example.jaewoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaewooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaewooApplication.class, args);
+	}
+
+}
---
+++ jaewoo/src/test/java/com/example/jaewoo/JaewooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jaewoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaewooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
